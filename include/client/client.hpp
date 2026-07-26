35a1688e6843fd86a807313f02b16097f07d0337
Ankeet1729 <ankeet1728@gmail.com>
2026-07-26 18:18:33 +0200

    started moving towards a refactor, client.hpp done

---
+++ include/client/client.hpp
@@ -0,0 +1,20 @@
+# pragma once
+
+#include <string>
+#include "../common/socket.hpp"
+
+class EchoClient {
+    
+
+    private:
+        Socket socket;
+        const std::string server_address;
+        const int server_port;
+        void connectToServer();
+        std::string receiveMessage();
+        void sendMessage(const std::string& message);
+    
+    public:
+        EchoClient(const std::string& server_address, const int server_port);
+        void start();
+};
